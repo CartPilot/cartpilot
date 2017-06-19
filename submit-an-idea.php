21e9c07282ac265523e727bfc81186b66f6a8ad8
Clay Teufel <clayteufel@gmail.com>
2017-06-19 10:35:30 -0500

    login

---
+++ submit-an-idea.php
@@ -0,0 +1,51 @@
+<?php require "login/loginheader.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>CartPilot | Submit an Idea</title>
+                    <link href="css/bootstrap.min.css" rel="stylesheet">
+                    <link href="css/custom.css" rel="stylesheet">
+                        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+                        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+                        <!--[if lt IE 9]>
+                         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+                         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+                         <![endif]-->
+                        </head>
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.html">CartPilot</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="feed.html">Feed</a></li>
+                        <li class="active"><a href="submit-an-idea.html">Submit an Idea<span class="sr-only">(current)</span></a></li>
+                        <li><a href="about-us.html">About Us</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></a></li>
+                        <li><a href="log-in.html">Log In</a></li>
+                        <li><a href="register.html">Create an Account</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+        <div>
+            <h1 class="text-center">Submit an Idea</h1>
+            <p class="text-center">Let's get started.</p>
+            <p class="text-center"><a class="btn btn-primary btn-lg" href="log-in.html" role="button">Log In</a><a class="btn btn-primary btn-lg" href="register.html" role="button">Create an Account</a></p>
+            <p class="text-center"><a href="/form/form-intro.html">Skip this step</a></p>
+        </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
